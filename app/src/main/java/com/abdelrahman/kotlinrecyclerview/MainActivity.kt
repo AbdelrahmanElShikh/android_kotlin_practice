f0f5afa832d2997c58de6f16d09ea878411b1b10
Abdel-Rahman <a.rahmanelshikh@gmail.com>
2019-09-02 18:15:14 +0200

    finish recycler with dummy data.

--- app/src/main/java/com/abdelrahman/kotlinrecyclerview/MainActivity.kt
+++ app/src/main/java/com/abdelrahman/kotlinrecyclerview/MainActivity.kt
@@ -2,11 +2,36 @@ package com.abdelrahman.kotlinrecyclerview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.abdelrahman.kotlinrecyclerview.adapters.OrderAdapter
+import com.abdelrahman.kotlinrecyclerview.databinding.ActivityMainBinding
+import com.abdelrahman.kotlinrecyclerview.models.Order
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val adapter = OrderAdapter(generateData())
+        val layoutManager = LinearLayoutManager(applicationContext)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.itemAnimator = DefaultItemAnimator()
+        binding.recyclerView.adapter = adapter
+        adapter.notifyDataSetChanged()
+
+    }
+
+    private fun generateData(): ArrayList<Order> {
+        val result = ArrayList<Order>()
+
+        for (i in 0..9) {
+            val order= Order("Name $i", "Address" , "22/4/1997")
+            result.add(order)
+        }
+
+        return result
     }
 }
--- app/src/main/java/com/abdelrahman/kotlinrecyclerview/adapters/OrderAdapter.kt
+++ app/src/main/java/com/abdelrahman/kotlinrecyclerview/adapters/OrderAdapter.kt
@@ -1,10 +1,8 @@
 package com.abdelrahman.kotlinrecyclerview.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.abdelrahman.kotlinrecyclerview.R
 import com.abdelrahman.kotlinrecyclerview.databinding.ListItemBinding
 import com.abdelrahman.kotlinrecyclerview.models.Order
 
@@ -12,21 +10,22 @@ import com.abdelrahman.kotlinrecyclerview.models.Order
 /**
 @author  Abdel-Rahman El-Shikh on 02-Sep-19.
  */
-class OrderAdapter(private var orders: ArrayList<Order>): RecyclerView.Adapter<OrderAdapter.ViewHolder>() {
+class OrderAdapter(private var orders: ArrayList<Order>) :
+    RecyclerView.Adapter<OrderAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val itemView = LayoutInflater.from(parent?.context).inflate(R.layout.list_item,parent,false)
-            return ViewHolder(itemView)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ListItemBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        return orders.size
+        return this.orders.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var order = orders.get(position)
+        val order = orders.get(position)
+        holder.binding.order = order
     }
 
-    class ViewHolder(viewItem: View ) : RecyclerView.ViewHolder(viewItem) {
-        lateinit var binding: ListItemBinding
-    }
+    class ViewHolder(var binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
 }
